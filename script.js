72f576e53fe0438b1fbf1acb4ebbb632fcc040c9
ap5089393 <ap5089393@gmail.com>
2026-01-17 16:40:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-const videosEl = document.getElementById("videos");
+fetch("https://NAMA-WORKER.kamu.workers.dev/list")
+  .then(res => res.json())
+  .then(data => {
+    const box = document.getElementById("videoList");
 
-async function loadVideos() {
-  const res = await fetch(
-    `https://doodapi.com/api/file/list?key=${DOOD_API_KEY}&per_page=20`
-  );
-  const data = await res.json();
-
-  if (!data.result) {
-    videosEl.innerHTML = "Gagal load video";
-    return;
-  }
-
-  videosEl.innerHTML = data.result.files.map(v => `
-    <div class="card">
-      <img src="${v.single_img}" />
-      <h3>${v.title}</h3>
-      <a href="player.html?id=${v.file_code}">Play</a>
-    </div>
-  `).join("");
-}
-
-loadVideos();
+    data.result.forEach(v => {
+      box.innerHTML += `
+        <div class="card">
+          <img src="${v.splash_img}">
+          <h4>${v.title}</h4>
+          <a href="play.html?id=${v.file_code}" style="color:#0f0">Play</a>
+        </div>
+      `;
+    });
+  })
+  .catch(err => {
+    document.getElementById("videoList").innerHTML = "Gagal load video";
+    console.error(err);
+  });
